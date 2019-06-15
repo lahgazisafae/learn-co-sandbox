e64e6ff9b545ef50c3a1a4e4bf11a57b33a2d01e
Safae Lahgazi Alaoui <safae.alaoui@flatironschool.com>
2019-06-15 17:21:27 +0000

    Automatically backed up by Learn

--- methods.rb
+++ methods.rb
@@ -8,3 +8,12 @@ def print_student_names
   puts "Charleese"
   puts "Jon"
 end
+
+  puts "Safae"
+  puts "Gary"
+  puts "Paris"
+  puts "Courtney"
+  puts "Danny"
+  puts "Martin"
+  puts "Charleese"
+  puts "Jon"
