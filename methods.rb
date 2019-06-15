8cff6a487b1c5eabe5e77b87ee5befdc6cc99a38
Safae Lahgazi Alaoui <safae.alaoui@flatironschool.com>
2019-06-15 17:50:05 +0000

    Automatically backed up by Learn

--- methods.rb
+++ methods.rb
@@ -1,43 +1,43 @@
-def print_student_names
-  puts "Safae"
-  puts "Gary"
-  puts "Paris"
-  puts "Courtney"
-  puts "Danny"
-  puts "Martin"
-  puts "Charleese"
-  puts "Jon"
-end
+# def print_student_names
+#   puts "Safae"
+#   puts "Gary"
+#   puts "Paris"
+#   puts "Courtney"
+#   puts "Danny"
+#   puts "Martin"
+#   puts "Charleese"
+#   puts "Jon"
+# end
 
-def  print_students(first,second, third, fourth)
-  puts first
-  puts second
-  puts third
-  puts fourth
-end
+# def  print_students(first,second, third, fourth)
+#   puts first
+#   puts second
+#   puts third
+#   puts fourth
+# end
 
-print_students("safae", "gary", "paris", "Courtney")
+# print_students("safae", "gary", "paris", "Courtney")
 
-print_student_names
+# print_student_names
 
 
-def print_hello_world
-  puts "Hello World!"
-end 
+# def print_hello_world
+#   puts "Hello World!"
+# end 
 
-#print_hello_world
+# #print_hello_world
 
-def print_hello_name( name )
+# def print_hello_name( name )
   
-  puts "Hello " + name  
+#   puts "Hello " + name  
 
-end
+# end
 
-print_hello_name("Martin")
+# print_hello_name("Martin")
 
-print_hello_name("6")
+# print_hello_name("6")
 
-name = "safae"
+# name = "safae"
 
 print_hello_name(name)
 
